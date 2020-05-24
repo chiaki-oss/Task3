a155f58c59090b5fc7ba8a5c5aa1c77530993bd3
渡辺　千晶 <chiaki@watanabechiakinoMacBook-Air.local>
2020-05-24 19:55:41 +0900

    Complete[Header](83)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
+  get 'books/new'
+  get 'books/edit'
   devise_for :users
+  root 'books#index' #ログイン認証後、一覧画面へ
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

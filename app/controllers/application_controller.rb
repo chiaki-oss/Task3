2d0e614cb876731897a31dcc2f028e317fa52f99
渡辺　千晶 <chiaki@watanabechiakinoMacBook-Air.local>
2020-05-25 17:33:37 +0900

    [Mypage](68)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
+	#ログインしてなければログイン画面へ
+	before_action :authenticate_user!
     #ユーザー登録時にnameのデータ操作を許可
 	before_action :configure_permitted_parameters, if: :devise_controller?
 	protected
 	def configure_permitted_parameters
-		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :introduction])
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # get 'users/show'
   get 'books/index'
   get 'books/show'
   get 'books/new'
@@ -6,5 +7,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'books#index' #ログイン認証後、一覧画面へ
   resources :books
+  resources :users, only:[:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
